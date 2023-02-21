4042a4e0cf6a2843c38375df000b7b29ebd73914
Norman Aranez <araneznorman12@gmail.com>
2023-02-21 23:10:14 +0800

    add test

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ const People = React.lazy(() => import('./views/people/People'))
 
 const routes = [
   { path: '/', exact: true, name: 'Home', element: People },
-  { path: '/people', name: 'People', element: People },
+  // { path: '/people', name: 'People', element: People },
 ]
 
 export default routes
